e1cbbe42228311d18c0387b909388d8143e5d0f6
alisa <alisadavina1010@gmail.com>
2024-08-09 17:04:23 +0700

    p

--- resources/views/membercash/membercashier.blade.php
+++ resources/views/membercash/membercashier.blade.php
@@ -17,7 +17,7 @@
                 <h6>Member</h6>
               </div>
                 <div class="card-body px-0 pb-2">
-                  <div class="table-responsive p-0">
+                  <div class="table-responsive p-0"> 
                     <table class="table align-items-center mb-0" id="datatable">
                       <thead>
                         <tr>
