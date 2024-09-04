7f1c8a19b57702dde0f354f11f457dae52c33834
Muhammad Fachril Irham <222310234@smkn1padaherang.sch.id>
2024-09-04 14:31:25 +0700

    update dashboard admin

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -3,97 +3,89 @@
         <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
             <div class="card">
                 <div class="card-header p-3 pt-2">
-                    <div
-                        class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                    <div class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
                         <i class="material-icons opacity-10">weekend</i>
                     </div>
                     <div class="text-end pt-1">
                         <p class="text-sm mb-0 text-capitalize">Today's Money</p>
-                        <h4 class="mb-0">$53k</h4>
+                        <h4 class="mb-0">Rp. {{ number_format($todaysMoney, 0, ',', '.') }}</h4>
                     </div>
                 </div>
                 <hr class="dark horizontal my-0">
                 <div class="card-footer p-3">
-                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than last
-                        week</p>
+                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">{{ round($todaysMoneyComparison, 2) }}% </span>than yesterday</p>
                 </div>
             </div>
         </div>
         <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
             <div class="card">
                 <div class="card-header p-3 pt-2">
-                    <div
-                        class="icon icon-lg icon-shape bg-gradient-primary shadow-primary text-center border-radius-xl mt-n4 position-absolute">
+                    <div class="icon icon-lg icon-shape bg-gradient-primary shadow-primary text-center border-radius-xl mt-n4 position-absolute">
                         <i class="material-icons opacity-10">person</i>
                     </div>
                     <div class="text-end pt-1">
                         <p class="text-sm mb-0 text-capitalize">Today's Users</p>
-                        <h4 class="mb-0">2,300</h4>
+                        <h4 class="mb-0">{{ number_format($todaysUsers, 0, ',', '.') }}</h4>
                     </div>
                 </div>
                 <hr class="dark horizontal my-0">
                 <div class="card-footer p-3">
-                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+3% </span>than last
-                        month</p>
+                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">{{ round($todaysUsersComparison, 2) }}% </span>than yesterday</p>
                 </div>
             </div>
         </div>
         <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
             <div class="card">
                 <div class="card-header p-3 pt-2">
-                    <div
-                        class="icon icon-lg icon-shape bg-gradient-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
+                    <div class="icon icon-lg icon-shape bg-gradient-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
                         <i class="material-icons opacity-10">person</i>
                     </div>
                     <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">New Clients</p>
-                        <h4 class="mb-0">3,462</h4>
+                        <p class="text-sm mb-0 text-capitalize">New Members</p>
+                        <h4 class="mb-0">{{ number_format($newMembers, 0, ',', '.') }}</h4>
                     </div>
                 </div>
                 <hr class="dark horizontal my-0">
                 <div class="card-footer p-3">
-                    <p class="mb-0"><span class="text-danger text-sm font-weight-bolder">-2%</span> than yesterday
-                    </p>
+                    <p class="mb-0"><span class="text-danger text-sm font-weight-bolder">{{ round($newMembersComparison, 2) }}%</span> than yesterday</p>
                 </div>
             </div>
         </div>
         <div class="col-xl-3 col-sm-6">
             <div class="card">
                 <div class="card-header p-3 pt-2">
-                    <div
-                        class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute">
+                    <div class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute">
                         <i class="material-icons opacity-10">weekend</i>
                     </div>
                     <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">Sales</p>
-                        <h4 class="mb-0">$103,430</h4>
+                        <p class="text-sm mb-0 text-capitalize">Total Sales</p>
+                        <h4 class="mb-0">Rp. {{ number_format($totalSales, 0, ',', '.') }}</h4>
                     </div>
                 </div>
                 <hr class="dark horizontal my-0">
                 <div class="card-footer p-3">
-                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+5% </span>than
-                        yesterday</p>
+                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">{{ round($salesComparison, 2) }}% </span>than yesterday</p>
                 </div>
             </div>
         </div>
-    </div>
+    </div>    
     <div class="row mt-4">
         <div class="col-lg-4 col-md-6 mt-4 mb-4">
             <div class="card z-index-2">
                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
                     <div class="bg-gradient-lightblue shadow-lightblue border-radius-lg py-3 pe-1">
-                        <div class="chart">
+                        <div class="chart mt-3">
                             <canvas id="chart-bars" class="chart-canvas" height="170"></canvas>
                         </div>
                     </div>
                 </div>
                 <div class="card-body">
-                    <h6 class="mb-0">Website Views</h6>
-                    <p class="text-sm">Last Campaign Performance</p>
+                    <h6 class="mb-0">Orders This Week</h6>
+                    <p class="text-sm">Count of orders for the last 7 days</p>
                     <hr class="dark horizontal">
                     <div class="d-flex">
                         <i class="material-icons text-sm my-auto me-1">schedule</i>
-                        <p class="mb-0 text-sm"> campaign sent 2 days ago </p>
+                        <p class="mb-0 text-sm">Updated daily</p>
                     </div>
                 </div>
             </div>
@@ -102,18 +94,18 @@
             <div class="card z-index-2">
                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
                     <div class="bg-gradient-lightgreen shadow-lightgreen border-radius-lg py-3 pe-1">
-                        <div class="chart">
+                        <div class="chart mt-3">
                             <canvas id="chart-line" class="chart-canvas" height="170"></canvas>
                         </div>
                     </div>
                 </div>
                 <div class="card-body">
-                    <h6 class="mb-0">Daily Sales</h6>
-                    <p class="text-sm">(<span class="font-weight-bolder">+15%</span>) increase in today sales.</p>
+                    <h6 class="mb-0">Payments This Week</h6>
+                    <p class="text-sm">Count of payments for the last 7 days</p>
                     <hr class="dark horizontal">
                     <div class="d-flex">
                         <i class="material-icons text-sm my-auto me-1">schedule</i>
-                        <p class="mb-0 text-sm"> updated 4 min ago </p>
+                        <p class="mb-0 text-sm">Updated daily</p>
                     </div>
                 </div>
             </div>
@@ -122,18 +114,18 @@
             <div class="card z-index-2">
                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
                     <div class="bg-gradient-lightcoral shadow-lightcoral border-radius-lg py-3 pe-1">
-                        <div class="chart">
+                        <div class="chart mt-3">
                             <canvas id="chart-line-tasks" class="chart-canvas" height="170"></canvas>
                         </div>
                     </div>
                 </div>
                 <div class="card-body">
-                    <h6 class="mb-0">Completed Tasks</h6>
-                    <p class="text-sm">Last Campaign Performance</p>
+                    <h6 class="mb-0">New Members This Week</h6>
+                    <p class="text-sm">Count of new members for the last 7 days</p>
                     <hr class="dark horizontal">
                     <div class="d-flex">
                         <i class="material-icons text-sm my-auto me-1">schedule</i>
-                        <p class="mb-0 text-sm">just updated</p>
+                        <p class="mb-0 text-sm">Updated daily</p>
                     </div>
                 </div>
             </div>
@@ -148,10 +140,10 @@
         new Chart(ctx1, {
             type: 'bar',
             data: {
-                labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+                labels: {!! json_encode($datesWeekly) !!},
                 datasets: [{
-                    label: 'Website Views',
-                    data: [50, 40, 35, 30, 20, 15, 10],
+                    label: 'Orders',
+                    data: {!! json_encode($ordersData) !!},
                     backgroundColor: 'rgb(36, 111, 154)'
                 }]
             },
@@ -168,10 +160,10 @@
         new Chart(ctx2, {
             type: 'line',
             data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                labels: {!! json_encode($datesWeekly) !!},
                 datasets: [{
-                    label: 'Daily Sales',
-                    data: [100, 80, 50, 40, 30, 20, 10],
+                    label: 'Payments',
+                    data: {!! json_encode($paymentsData) !!},
                     backgroundColor: 'rgba(40, 167, 69, 0.7)',
                     borderColor: 'rgba(40, 167, 69, 1)',
                     borderWidth: 1,
@@ -191,10 +183,10 @@
         new Chart(ctx3, {
             type: 'line',
             data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                labels: {!! json_encode($datesWeekly) !!},
                 datasets: [{
-                    label: 'Completed Tasks',
-                    data: [10, 20, 30, 50, 70, 80, 100],
+                    label: 'New Members',
+                    data: {!! json_encode($membersData) !!},
                     backgroundColor: 'rgba(255, 145, 10, 0.4)',
                     borderColor: 'rgba(255, 99, 71, 0.6)',
                     borderWidth: 1,
