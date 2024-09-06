e2bd5a4c8c6f29c8f4f2a1e474ae90ca72672f02
Muhammad Fachril Irham <222310234@smkn1padaherang.sch.id>
2024-09-06 16:34:11 +0700

    update dashboard

--- resources/views/dashboard/master.blade.php
+++ resources/views/dashboard/master.blade.php
@@ -49,6 +49,8 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/2.1.2/css/dataTables.bootstrap5.css" rel="stylesheet">
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
     <style>
         body {
             font-family: 'Poppins', sans-serif;
