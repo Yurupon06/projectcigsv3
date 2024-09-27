c257a9c2e751cb03f961cb340b0814b2fbf3ef27
Muhammad Fachril Irham <222310234@smkn1padaherang.sch.id>
2024-09-27 13:13:46 +0700

    add complement to myorder

--- resources/views/landing/complement/detail.blade.php
+++ resources/views/landing/complement/detail.blade.php
@@ -1,129 +1,187 @@
 @extends('landing.master')
 @section('main')
 
-<body class="animsition">
+    <body class="animsition">
 
-    <!-- Product Detail Section -->
-    <section class="sec-product-detail bg0 p-t-65 p-b-60">
-        <div class="container">
-            <div class="row">
-                <!-- Image Section -->
-                <div class="col-md-6 col-lg-7 p-b-30">
-                    <div class="p-l-25 p-r-30 p-lr-0-lg">
-                        <div class="wrap-pic-w pos-relative" style="overflow: hidden;">
-                            <img src="{{ asset('storage/' . $complement->image) }}" alt="IMG-PRODUCT" class="img-fluid" style="border-radius: 10px;">
+        <!-- Product Detail Section -->
+        <section class="sec-product-detail bg0 p-t-65 p-b-60">
+            <div class="container">
+                <div class="row">
+                    <!-- Image Section -->
+                    <div class="col-md-6 col-lg-7 p-b-30">
+                        <div class="p-l-25 p-r-30 p-lr-0-lg">
+                            <div class="wrap-pic-w pos-relative" style="overflow: hidden;">
+                                <img src="{{ asset('storage/' . $complement->image) }}" alt="IMG-PRODUCT" class="img-fluid"
+                                    style="border-radius: 10px;">
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <!-- Product Details Section -->
-                <div class="col-md-6 col-lg-5 p-b-30">
-                    <div class="p-r-50 p-t-5 p-lr-0-lg ms-2">
-                        <!-- Product Name -->
-                        <h4 class="mtext-105 cl2 js-name-detail p-b-14" style="font-size: 32px; font-weight: bold; color: #333;">
-                            {{ $complement->name }}
-                        </h4>
-                        
-                        <!-- Product Price -->
-                        <span class="stext-105 cl3" style="font-size: 26px; color: #ffa500;">
-                            Rp. {{ number_format($complement->price) }}
-                        </span><br>
+                    <!-- Product Details Section -->
+                    <div class="col-md-6 col-lg-5 p-b-30">
+                        <div class="p-r-50 p-t-5 p-lr-0-lg ms-2">
+                            <!-- Product Name -->
+                            <h4 class="mtext-105 cl2 js-name-detail p-b-14"
+                                style="font-size: 32px; font-weight: bold; color: #333;">
+                                {{ $complement->name }}
+                            </h4>
 
-                        <!-- Product Stock -->
-                        <span class="stext-100 cl3" style="font-size: 16px; color: #666;">
-                            Stok: {{ $complement->stok }}
-                        </span>
+                            <!-- Product Price -->
+                            <span class="stext-105 cl3" style="font-size: 26px; color: #ffa500;">
+                                Rp. {{ number_format($complement->price) }}
+                            </span><br>
 
-                        <!-- Product Description -->
-                        <p class="stext-102 cl3 p-t-23" style="font-size: 16px; line-height: 1.8; color: #777;">
-                            {{ $complement->description }}
-                        </p>
+                            <!-- Product Stock -->
+                            <span class="stext-100 cl3" style="font-size: 16px; color: #666;">
+                                Stok: <span class="stock">{{ $complement->stok }}</span>
+                            </span>
 
-                        <!-- Add to Cart Section -->
-                        <div class="d-flex align-items-center justify-content-end pt-2">
-                            <form action="{{ route('cart.add', $complement->id) }}" method="POST">
-                                @csrf
-                                <div class="wrap-num-product flex-w m-r-20 m-tb-10" style="border-radius: 10px; overflow: hidden;">
+                            <!-- Product Description -->
+                            <p class="stext-102 cl3 p-t-23" style="font-size: 16px; line-height: 1.8; color: #777;">
+                                {{ $complement->description }}
+                            </p>
+
+                            <!-- Add to Cart Section -->
+                            <div class="d-flex align-items-center justify-content-end pt-2">
+                                <form action="{{ route('cart.add', $complement->id) }}" method="POST">
+                                    @csrf
+                                    {{-- <div class="wrap-num-product flex-w m-r-20 m-tb-10" style="border-radius: 10px; overflow: hidden;">
                                     <div class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
                                         <i class=" fs-16 zmdi zmdi-minus"></i>
                                     </div>
                             
-                                    <input class="mtext-104 cl3 txt-center num-product" type="number" name="quantity" value="1" min="1" max="{{ $complement->stok }}">
+                                    <input class="mtext-104 cl3 txt-center" type="number" name="quantity" id="quantity" value="1" min="1" max="{{ $complement->stok }}">
                             
                                     <div class="btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m">
                                         <i class="fs-16 zmdi zmdi-plus"></i>
                                     </div>
-                                </div>
+                                </div> --}}
 
-                                <!-- Add to Cart Button -->
-                                <button type="submit" class="addcart flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn3 p-lr-15 trans-04 js-addcart-detail">
-                                    Add to Cart
-                                </button>
-                            </form>
+                                    <div class="d-flex justify-content-between p-3">
+                                        <div class="minus">
+                                            <i class="bi bi-dash fs-3"></i>
+                                        </div>
+                                        <input class="text-center" type="number" name="quantity" id="quantity"
+                                            value="1" min="1" max="{{ $complement->stok }}" readonly>
+                                        <div class="plus">
+                                            <i class="bi bi-plus fs-3"></i>
+                                        </div>
+                                    </div>
+
+                                    <!-- Add to Cart Button -->
+                                    <button type="submit"
+                                        class="addcart flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn3 p-lr-15 trans-04 js-addcart-detail">
+                                        Add to Cart
+                                    </button>
+                                </form>
+
+                            </div>
+                            <div class="total-price">Total: Rp. <span id="total-price"></span></div>
+
                         </div>
                     </div>
                 </div>
             </div>
+        </section>
+
+        <!-- Back to Top Button -->
+        <div class="btn-back-to-top" id="myBtn">
+            <span class="symbol-btn-back-to-top">
+                <i class="zmdi zmdi-chevron-up"></i>
+            </span>
         </div>
-    </section>
 
-    <!-- Back to Top Button -->
-    <div class="btn-back-to-top" id="myBtn">
-        <span class="symbol-btn-back-to-top">
-            <i class="zmdi zmdi-chevron-up"></i>
-        </span>
-    </div>
-
-    <style>
-        /* Custom CSS for responsive adjustments */
-        .wrap-pic-w {
-            position: relative;
-            overflow: hidden;
-            border-radius: 10px;
-        }
-        
-        .btn-num-product-down,
-        .btn-num-product-up {
-            border-radius: 5px;
-        }
-
-        .btn-num-product-down:hover {
-            background-color: #ffa500;
-            color: #000;
-        }
-        
-        .btn-num-product-up:hover {
-            background-color: #ffa500;
-            color: #000;
-        }
-
-        .addcart {
-            background-color: #000;
-            color: #fff;
-            border-radius: 5px;
-            padding: 5px;
-            margin: 5px;
-        }
-
-        .addcart:hover {
-            background-color: #ffa500;
-            color: #fff;
-        }
-
-        @media (max-width: 767.98px) {
-            .size-204 {
-                width: 100%;
+        <style>
+            /* Custom CSS for responsive adjustments */
+            .wrap-pic-w {
+                position: relative;
+                overflow: hidden;
+                border-radius: 10px;
             }
 
-            .p-r-50 {
-                padding-right: 15px;
+            .btn-num-product-down,
+            .btn-num-product-up {
+                border-radius: 5px;
             }
 
-            .slick3 img {
-                max-width: 100%;
-                height: auto;
+            .btn-num-product-down:hover {
+                background-color: #ffa500;
+                color: #000;
             }
-        }
-    </style>
-</body>
+
+            .btn-num-product-up:hover {
+                background-color: #ffa500;
+                color: #000;
+            }
+
+            .addcart {
+                background-color: #000;
+                color: #fff;
+                border-radius: 5px;
+                padding: 5px;
+                margin: 5px;
+            }
+
+            .addcart:hover {
+                background-color: #ffa500;
+                color: #fff;
+            }
+
+            @media (max-width: 767.98px) {
+                .size-204 {
+                    width: 100%;
+                }
+
+                .p-r-50 {
+                    padding-right: 15px;
+                }
+
+                .slick3 img {
+                    max-width: 100%;
+                    height: auto;
+                }
+            }
+        </style>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                const price = {{ $complement->price }};
+                let stock = {{ $complement->stok }};
+
+                function updateTotal() {
+                    const quantity = parseInt($('#quantity').val());
+                    const total = price * quantity;
+                    $('#total-price').text(new Intl.NumberFormat('id-ID').format(total));
+                }
+
+                function updateStockDisplay() {
+                    const quantity = parseInt($('#quantity').val());
+                    $('.stock').text(stock - quantity);
+                }
+
+                $('.minus').click(function() {
+                    let quantity = parseInt($('#quantity').val());
+                    if (quantity > 1) {
+                        $('#quantity').val(quantity - 1);
+                        stock + 1;
+                        updateTotal();
+                        updateStockDisplay();
+                    }
+                });
+
+                $('.plus').click(function() {
+                    let quantity = parseInt($('#quantity').val());
+                    if (quantity < stock) {
+                        $('#quantity').val(quantity + 1);
+                        stock - 1;
+                        updateTotal();
+                        updateStockDisplay();
+                    }
+                });
+
+                updateStockDisplay();
+                updateTotal();
+            });
+        </script>
+    </body>
 @endsection
--- resources/views/landing/order.blade.php
+++ resources/views/landing/order.blade.php
@@ -2,7 +2,7 @@
 @section('title', isset($setting) ? $setting->app_name . ' - Order' : 'Order')
 @section('main')
 
-    @if(session('success'))
+    @if (session('success'))
         <div class="alert alert-success" role="alert">
             {{ session('success') }}
         </div>
@@ -12,14 +12,17 @@
         .status-badge {
             font-weight: bold;
         }
+
         .card {
             cursor: pointer;
             transition: transform 0.3s ease, box-shadow 0.3s ease;
         }
+
         .card:hover {
             transform: scale(1.02);
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
         }
+
         @media (max-width: 576px) {
             .card-text {
                 font-size: 14px;
@@ -27,10 +30,10 @@
         }
 
         /* Responsive Search Form */
-        .search-form input{
+        .search-form input {
             border-radius: 0 0 0 0;
         }
-        
+
         .search-form button {
             border-radius: 0 20px 20px 0;
         }
@@ -40,6 +43,7 @@
             .search-form input {
                 width: 50%;
             }
+
             .search-form button {
                 width: 30%;
                 border-radius: 0 20px 20px 0;
@@ -51,11 +55,13 @@
                 width: 60%;
                 font-size: 14px;
             }
+
             .search-form button {
                 width: 40%;
                 font-size: 14px;
                 border-radius: 0 20px 20px 0;
             }
+
             .search-form input[type="date"] {
                 position: relative;
 
@@ -66,86 +72,91 @@
             padding-bottom: 100px;
             margin-top: 80px;
         }
+
+        .btn-outline-orange {
+            border-color: #ff7e00;
+        }
+
+        .btn-outline-orange:hover {
+            background-color: #ff7e00;
+            color: #fff;
+        }
+
+        .btn-outline-orange.active {
+            background-color: #ff7e00;
+            border-color: #ff7e00;
+            color: #fff;
+        }
     </style>
 
     <div class="container">
+        <form method="GET" action="{{ route('yourorder.index') }}" id="filter-form" class="">
+            <div class="btn-group mb-4">
+                <input type="radio" name="type" id="membership" value="membership" class="visually-hidden" onchange="this.form.submit()">
+                <label for="membership" class="btn btn-outline-orange {{ request('type') == 'membership' ? 'active' : '' }}">Membership</label>
+                <input type="radio" name="type" id="complement" value="complement" class="visually-hidden" onchange="this.form.submit()">
+                <label for="complement" class="btn btn-outline-orange {{ request('type') == 'complement' ? 'active' : '' }}">Complement</label>
+            </div>
+        </form>
         <div class="d-flex justify-content-center mb-4">
             <form method="GET" action="{{ route('yourorder.index') }}" class="d-flex search-form">
-                <input clang="s" type="text" name="search" class="form-control" placeholder="Search" value="{{ request('search') }}" style="border-radius: 20px 0 0 20px; height: 38px; font-size: 14px;">
-                <input type="date" name="start_date" id="start_date" class="form-control" value="{{ request('start_date', $startDate) }}" max="{{ date('Y-m-d', strtotime('now')) }}" style="height: 38px;">
-                <input type="date" name="end_date" id="end_date" class="form-control" value="{{ request('end_date', $endDate) }}" max="{{ date('Y-m-d', strtotime('now')) }}" style="height: 38px;">
-                <button type="submit" class="btn btn-primary" style="background-color: #ff7e00; height: 38px; width: 30px;  ">
+                <input clang="s" type="text" name="search" class="form-control" placeholder="Search"
+                    value="{{ request('search') }}" style="border-radius: 20px 0 0 20px; height: 38px; font-size: 14px;">
+                <input type="date" name="start_date" id="start_date" class="form-control"
+                    value="{{ request('start_date', $startDate) }}" max="{{ date('Y-m-d', strtotime('now')) }}"
+                    style="height: 38px;">
+                <input type="date" name="end_date" id="end_date" class="form-control"
+                    value="{{ request('end_date', $endDate) }}" max="{{ date('Y-m-d', strtotime('now')) }}"
+                    style="height: 38px;">
+                <button type="submit" class="btn btn-primary"
+                    style="background-color: #ff7e00; height: 38px; width: 30px;  ">
                     <i class="fas fa-search align-middle d-flex justify-content-center"></i>
                 </button>
             </form>
         </div>
-        
+
         <div class="row">
-            <!-- Desktop view orders -->
-            <div class="col-12 d-none d-lg-block">
-                <div class="row">
-                    <div class="col-lg-12 mb-4">
-                        @forelse ($orders as $i => $order)
-                            <a href="{{ route('checkout', $order->id) }}" class="card mb-4 text-decoration-none text-dark">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{ $order->customer->user->name }}</h5>
-                                    <h6 class="card-subtitle mb-2 text-muted">{{ $order->product->product_name }}</h6>
-                                    <p class="card-text">
-                                        <strong>Order Date:</strong> {{ \Carbon\Carbon::parse($order->order_date)->translatedFormat('d F Y H:i') }}
-                                        <br>
-                                        <strong>Total Amount:</strong> Rp {{ number_format($order->total_amount) }}
-                                        <br>
-                                        <strong>Status:</strong> 
-                                        <span class="status-badge" style="color: {{ $order->status == 'unpaid' ? 'red' : ($order->status == 'canceled' ? 'gray' : 'green') }};">
-                                            {{ ucfirst($order->status) }}
-                                        </span>
-                                    </p>
-                                </div>
-                            </a>
-                        @empty
-                            <p class="text-center">No orders found.</p>
-                        @endforelse
-                        <div class="container-fluid">
-                            <div class="row">
-                                <div class="col-12">
-                                    {{ $orders->appends(request()->except('page'))->links('pagination::bootstrap-5') }}
+            @if (request('type') === 'membership')
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-lg-12 mb-4 list">
+                            @forelse ($orders as $order)
+                                <a href="{{ route('checkout', $order->id) }}" class="card mb-4 text-decoration-none text-dark">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $order->customer->user->name }}</h5>
+                                        <h6 class="card-subtitle mb-2 text-muted">{{ $order->product->product_name }}</h6>
+                                        <p class="card-text">
+                                            <strong>Order Date:</strong>
+                                            {{ \Carbon\Carbon::parse($order->order_date)->translatedFormat('d F Y H:i') }}
+                                            <br>
+                                            <strong>Total Amount:</strong> Rp {{ number_format($order->total_amount) }}
+                                            <br>
+                                            <strong>Status:</strong>
+                                            <span class="status-badge"
+                                                style="color: {{ $order->status == 'unpaid' ? 'red' : ($order->status == 'canceled' ? 'gray' : 'green') }};">
+                                                {{ ucfirst($order->status) }}
+                                            </span>
+                                        </p>
+                                    </div>
+                                </a>
+                            @empty
+                                <p class="text-center">No orders found.</p>
+                            @endforelse
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="col-12">
+                                        {{ $orders->appends(request()->except('page'))->links('pagination::bootstrap-5') }}
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <!-- Mobile Layout -->
-            <div class="col-12 d-lg-none">
-                @forelse ($orders as $i => $order)
-                    <a href="{{ route('checkout', $order->id) }}" class="card mb-4 text-decoration-none text-dark">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $order->customer->user->name }}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">{{ $order->product->product_name }}</h6>
-                            <p class="card-text">
-                                <strong>Order Date:</strong> {{ \Carbon\Carbon::parse($order->order_date)->translatedFormat('d F Y H:i') }}
-                                <br>
-                                <strong>Total Amount:</strong> Rp {{ number_format($order->total_amount) }}
-                                <br>
-                                <strong>Status:</strong> 
-                                <span class="status-badge" style="color: {{ $order->status == 'unpaid' ? 'red' : ($order->status == 'canceled' ? 'gray' : 'green') }};">
-                                    {{ ucfirst($order->status) }}
-                                </span>
-                            </p>
-                        </div>
-                    </a>
-                @empty
-                    <p style="text-align: center; font-weight: bold;">No orders found.</p>
-                @endforelse
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-12 justify-content-end">
-                            {{ $orders->appends(request()->except('page'))->links('pagination::bootstrap-5') }}
-                        </div>
-                    </div>
+            @elseif (request('type') === 'complement')
+                <div class="col-12">
+                    <p class="text-center">Complement orders are currently not displayed.</p>
                 </div>
-            </div>
+            @endif
         </div>
     </div>
 @endsection
